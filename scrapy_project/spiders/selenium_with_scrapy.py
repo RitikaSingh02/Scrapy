99be0b88cd099b7ec3ba9f0b603b566f2cb63e59
RitikaSingh02 <ritika2002singh@gmail.com>
2021-02-09 13:16:09 +0530

    selenium with scarpy

---
+++ scrapy_project/spiders/selenium_with_scrapy.py
@@ -0,0 +1,32 @@
+import scrapy
+from selenium import webdriver
+from scrapy.selector import Selector
+
+
+class CoinSpiderSelenium(scrapy.Spider):
+    name = 'coin_selenium'
+    allowed_domains = ["www.livecoinwatch.com"]
+    start_urls = ["https://www.livecoinwatch.com/"]
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        driver = webdriver.Chrome(
+            "/home/ritika/Documents/selenium-env/env/scrapy/scrapy_project/chromedriver", options=options)
+        driver.get("https://www.livecoinwatch.com/price/Litecoin-LTC")
+
+        # search_input = driver.find_element_by_xpath(
+        #     "(//input[@class='form-control main-small-search'][1])")
+        # search_input.send_keys("LTC")
+
+        self.html = driver.page_source
+        # print(self.html)
+        driver.close()
+
+    def parse(self, response):
+        resp = Selector(text=self.html)
+        for search in resp.xpath("(//*[@id='__next']/div/div[1]/main/div/div[6]/div/div[2]/div/div/section/div[2]/div[1]/table/tbody/tr)"):
+            yield{
+                'response': "LTC",
+                'search': search.xpath("./td[2]/div/div/a/text()").get()
+            }
